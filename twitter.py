12709c1047844b35f3b1124bd8efd477f9c21058
Wojciech Jurkowlaniec <wojciech.jurkowlaniec@f-secure.com>
2018-01-02 04:33:35 +0100

    twitter the modern way

--- twitter.py
+++ twitter.py
@@ -6,35 +6,20 @@ import config
 
 tweetsXiteration = 100  # Where 100 is the max
 dateTo = str(date.today())  # Inclusive (YYYY-MM-DD)
-dateFrom = str(date.today() - timedelta(days=7))  # Exclusive (YYYY-MM-DD)
+dateFrom = str(date.today() - timedelta(days=1))  # Exclusive (YYYY-MM-DD)
 
 
 def get_twitter_mention_count(keyword):
     done = False  # Must be false
     tw = Twython(config.secrets['twitter']['consumer_key'], config.secrets['twitter']['consumer_secret'],
                  config.secrets['twitter']['access_token'], config.secrets['twitter']['access_token_secret'])
+    results = tw.cursor(tw.search, q=keyword, count=tweetsXiteration, since=dateFrom, until=dateTo, result_type='mixed')
+    count = 0
+    for res in results:
+        count += 1
+    return count
 
-    response = tw.search(q=keyword, count=tweetsXiteration, since=dateFrom, until=dateTo, result_type='mixed')
-    countTweets = len(response['statuses'])
-    if not ('next_results' in response['search_metadata']):
-        done = True
-
-    while not done:
-        # Parsing information for maxID
-        parse1 = response['search_metadata']['next_results'].split("&")
-        parse2 = parse1[0].split("?max_id=")
-        parse3 = parse2[1]
-        maxID = parse3
-
-        # Twitter is queried (again, this time with the addition of 'max_id')
-        response = tw.search(q=keyword, count=tweetsXiteration, since=dateFrom, until=dateTo, max_id=maxID,
-                             include_entities=1, result_type='mixed')
-
-        countTweets = countTweets + len(response['statuses'])
-
-        if not ('next_results' in response['search_metadata']):
-            done = True
-    return countTweets
+    # TODO twython.exceptions.TwythonRateLimitError: Twitter API returned a 429 (Too Many Requests), Rate limit exceeded
 
 
 if __name__ == "__main__":
