9809f8407a55ba9e3d09b2d3441909b2896cf1f4
Wojciech Jurkowlaniec <wojciech.jurkowlaniec@f-secure.com>
2018-01-02 03:29:13 +0100

    get reddit subs mention

--- config.py
+++ config.py
@@ -1,10 +1,12 @@
-
 secret_file_location = "secrets.json"
-
 
 coins_info = {
     'eth': {
         'display_name': 'Etherum',
         'subreddits': ['etherium']
     }
+}
+
+reddit = {
+    'crypto_subs': ['darknetmarkets', 'bitcoin', 'ethereum', 'dogecoin', 'cryptocurrency', 'btc', 'bitcoinmarkets']
 }
--- reddit.py
+++ reddit.py
@@ -36,13 +36,47 @@ def get_crypto_popularity(crypto):
     return total_users
 
 
+def get_crypto_mention_in_subreddit(crypto, subreddit_name):
+    sub = reddit.subreddit(subreddit_name)
+    search_results = list(sub.search(crypto, time_filter='day'))
+    article_count = len(search_results)
+    total_score = 0
+    total_comments = 0
+    for x in search_results:
+        total_score += x.score
+        total_comments += x.num_comments
+    return {
+        'total_score': total_score,
+        'total_comments': total_comments,
+        'article_count': article_count
+    }
+
+
+def get_crypto_mention_in_subreddits(coin_name):
+    score = {
+        'total_score': 0,
+        'total_comments': 0,
+        'article_count': 0
+    }
+    for sub in config.reddit['crypto_subs']:
+        data = get_crypto_mention_in_subreddit(coin_name, sub)
+        for key in score.keys():
+            score[key] += data[key]
+    return score
+
+
 def get_all_crypto_info():
     output = {}
     for coin in config.coins_info.keys():
-        output[coin] = {'total_users': get_crypto_popularity(coin), 'active_users': get_subreddit_active_users(coin)}
+        output[coin] = {
+            'total_users': get_crypto_popularity(coin),
+            'active_users': get_subreddit_active_users(coin),
+            'mentions_in_subs': get_crypto_mention_in_subreddits(config.coins_info[coin]['display_name'])
+        }
     return output
 
 
 if __name__ == "__main__":
     init_reddit()
+    # print(get_crypto_mention_in_subreddit("raiblocks", 'cryptocurrency'))
     print(get_all_crypto_info())
